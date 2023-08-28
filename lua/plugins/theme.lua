6d653bc90f8de81116d29651a82bcd681d2951e6
Thomas Rayden <twrayden@gmail.com>
2023-08-28 09:22:25 +0800

    Import existing config

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+
+  { 'NLKNguyen/papercolor-theme' },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "PaperColor",
+    },
+  },
+
+  { 'xiyaowong/transparent.nvim' }
+
+}
