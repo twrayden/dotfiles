8125eca9f8372411fd356c9cff893ff4567d8374
Thomas Rayden <twrayden@gmail.com>
2023-10-16 09:51:09 +0800

    Add leap.nvim

--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -24,4 +24,12 @@ return {
 
     -- "gc" to comment visual regions/lines
     'numToStr/Comment.nvim',
+
+    {
+        'ggandor/leap.nvim',
+        dependencies = { 'tpope/vim-repeat' },
+        config = function()
+            require('leap').add_default_mappings()
+        end
+    }
 }
